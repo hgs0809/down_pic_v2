aaa0151706bbece1a95e162641afeb45950dcfea
hgs0809 <hgs0809@gmail.com>
2016-04-22 12:49:16 +0800

    Update rule_sample.py

--- pic/spiders/rule_sample.py
+++ pic/spiders/rule_sample.py
@@ -13,6 +13,7 @@ class UrlSpider(CrawlSpider):
     #    'http://www.dbmeinv.com/',
     #rules = (Rule(LinkExtractor(allow='http://news.163.com/16/0421/10/BL5T67RI00014Q4P.html'),callback='parse_item'),)
     rules = (Rule(LinkExtractor(allow='http://news.163.com/16/\d{4}/\d{2}/.*.html',restrict_xpaths=("//h3/a[@class='ac01']")),callback='parse_item'),)
+                                        #这里根据allow里面的规矩来查找ur，restrict_xpaths是辅助作用，来过滤必须符合这个要求的url
     #rules = (Rule(LinkExtractor(allow='http://money.*.html'),callback='parse_item'),)
     #rules = (Rule(LinkExtractor(allow="http://view.163.com/\d+.*.html",deny="http://view.163.com/16/0310/11/BHPS4MFS000156M4.html"),callback='parse_item'),)
     #rules = (Rule(LinkExtractor(allow=('http://ww1.sinaimg.cn/bmiddle/0060lm7Tgw1f33k9if9msj30dw0dw75k.jpg')),callback='parse_item'),)
